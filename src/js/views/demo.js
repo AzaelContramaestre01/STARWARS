8e4d8600565fad618321d74381f1db9fe1e39468
Azael Contramaestre <72152743+AzaelContramaestre01@users.noreply.github.com>
2021-01-19 11:54:13 -0300

    Update demo.js

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,43 +1,47 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
 import { Context } from "../store/appContext";
-
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "../../styles/demo.scss";
 
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
 
+	useEffect(() => {
+		actions.getcharacters();
+		actions.addFavorites();
+		actions.deleteFavorites();
+	}, []);
 	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
+		<div className="conatainer">
+			<div className="col-6">
+				<h3>Favs</h3>
+				{store.characters.map((elemento, index) => {
 					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
+						<div key={index}>
+							<p>{elemento.name}</p>
+							<button
+								className="ml-2 btn btn-primary"
+								onClick={() => actions.addFavorites(elemento.name)}>
+								asdf
 							</button>
-						</li>
+						</div>
 					);
 				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
+			</div>
+			<div className="col-6">
+				<h3>favs</h3>
+				{store.favorites.map((elemento, index) => {
+					return (
+						<div key={index} className="d-flex">
+							<p>{elemento}</p>
+							<button className="btn" onClick={() => actions.deleteFavorites(index)}>
+								<i className="fa fa-trash" />
+							</button>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
